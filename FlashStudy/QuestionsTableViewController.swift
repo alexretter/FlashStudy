57ffceae7920b20e3e204861edf3cbc1f8d97539
alexretter <alexretter@optonline.net>
2016-03-25 18:20:56 -0600

    Navigation fix/Constraint Updated.

--- FlashStudy/QuestionsTableViewController.swift
+++ FlashStudy/QuestionsTableViewController.swift
@@ -14,6 +14,8 @@ class QuestionsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationController?.navigationBarHidden = false
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
