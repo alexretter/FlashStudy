839326bf438b284a53eb0d1fc70ae483afc49f2e
alexretter <alexretter@optonline.net>
2016-04-11 15:10:44 -0600

    math fix

--- FlashStudy/QuizController.swift
+++ FlashStudy/QuizController.swift
@@ -15,16 +15,24 @@ class QuizController
         let knownCardsCount = knownCards.count
         let unknownCardsCount = unknownCards.count
         
-        if knownCardsCount < unknownCardsCount
+        if unknownCardsCount == 0
         {
-            let doubleScore = Double(knownCardsCount) / Double(unknownCardsCount)
+            return 100
+        }
+        else if knownCardsCount == 0
+        {
+            return 0
+        }
+        else if knownCardsCount > unknownCardsCount
+        {
+            let doubleScore = Double(unknownCardsCount) / Double(knownCardsCount)
             let intScore = Int(doubleScore * 100)
             print(intScore)
             return intScore
         }
         else
         {
-            let doubleScore = Double(unknownCardsCount) / Double(knownCardsCount)
+            let doubleScore = Double(knownCardsCount) / Double(unknownCardsCount)
             let intScore = Int(doubleScore * 100)
             print(intScore)
             return intScore
